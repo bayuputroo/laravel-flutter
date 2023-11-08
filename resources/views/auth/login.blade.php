d2a7e32efb715907e2ed5c2e41aa8fba718c81cc
bayuputroo <bayuputroous@gmailcom>
2023-11-08 21:46:03 +0700

    login function

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,6 +14,7 @@
 
         <div class="card-body">
             <form method="POST" action="{{ route('login') }}" class="needs-validation" novalidate="">
+                @csrf
                 <div class="form-group">
                     <label for="email">Email</label>
                     <input id="email" type="email"

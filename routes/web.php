ec6dd112794dd53b1df6412664f0b50cd40ff4f1
bayuputroo <bayuputroous@gmailcom>
2023-11-10 15:52:10 +0700

    setup authorization

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
 Route::middleware(['auth', 'verified'])->group(function () {
     route::get('home', function () {
         return view('layouts.app', ['type_menu' => '']);
-    })->name('home');
+    })->name('home')->middleware('can:dashboard');
 });
 
 //Route::get('/login', function () {

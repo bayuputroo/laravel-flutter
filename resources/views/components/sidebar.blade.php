6ba712d6377447334734b219b694b7ed85ab6898
bayuputroo <bayuputroous@gmailcom>
2023-11-11 01:39:49 +0700

    add route change dan membuat model poduct dan Category

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,30 +1,24 @@
 <div class="main-sidebar sidebar-style-2">
     <aside id="sidebar-wrapper">
         <div class="sidebar-brand">
-            <a href="index.html">FIC Batch 7</a>
+            <a href="#">Stisla</a>
         </div>
         <div class="sidebar-brand sidebar-brand-sm">
-            <a href="index.html">FB7</a>
+            <a href="#">St</a>
         </div>
         <ul class="sidebar-menu">
-            <li class="nav-item dropdown">
-                <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
-                <ul class="dropdown-menu">
-                    <li class='{{ Request::is(' dashboard-general-dashboard') ? 'active' : '' }}'>
-                        <a class="nav-link" href="{{ url('dashboard-general-dashboard') }}">General Dashboard</a>
-                    <li class="{{ Request::is('dashboard-ecommerce-dashboard') ? 'active' : '' }}">
-                        <a class="nav-link" href="{{ url('dashboard-ecommerce-dashboard') }}">Ecommerce Dashboard</a>
-                    </li>
-                </ul>
-            </li>
-            <li class="nav-item ">
-                <a href="" class="nav-link "><i class="fas fa-users"></i><span>User List</span>
-            </li>
+            <li class="nav-item">
+                <a href="{{ route('home') }}" class="nav-link"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+        </ul>
+        </li>
+        <li class="nav-item ">
+            <a href="" class="nav-link "><i class="fas fa-users"></i><span>User List</span>
+        </li>
 
-            <div class="hide-sidebar-mini mt-4 mb-4 p-3">
-                <a href="https://getstisla.com/docs" class="btn btn-primary btn-lg btn-block btn-icon-split">
-                    <i class="fas fa-rocket"></i> Documentation
-                </a>
-            </div>
+        <div class="hide-sidebar-mini mt-4 mb-4 p-3">
+            <a href="https://getstisla.com/docs" class="btn btn-primary btn-lg btn-block btn-icon-split">
+                <i class="fas fa-rocket"></i> Documentation
+            </a>
+        </div>
     </aside>
 </div>

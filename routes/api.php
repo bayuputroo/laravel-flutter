c5131cf9baffe2ab71083157f8c0d01b46119152
bayuputroo <bayuputroous@gmailcom>
2023-11-11 02:27:16 +0700

    setup login dan setup laravel sanctum

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,5 +21,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('/login', [AuthController::class, 'login']);
 Route::apiResource('categories', CategoryController::class);
 Route::apiResource('products', ProductController::class);
